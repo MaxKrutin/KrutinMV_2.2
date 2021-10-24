5201bcae735836c40e2719e12c7ce9184f9de6f4
Max Krutin <maxkrutin@icloud.com>
2021-10-24 22:37:45 +0300

    My work 2.2

--- KrutinMV_2.2/ViewController.swift
+++ KrutinMV_2.2/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redCounter: UILabel!
+    @IBOutlet var greenCounter: UILabel!
+    @IBOutlet var blueCounter: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        redSlider.value = 0
+        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        greenSlider.value = 0
+        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        blueSlider.value = 0
+        
+        redCounter.text = String(round(redSlider.value * 100) / 100)
+        greenCounter.text = String(round(greenSlider.value * 100) / 100)
+        blueCounter.text = String(round(blueSlider.value * 100) / 100)
+        
     }
 
+    @IBAction func redSliderAction() {
+        redCounter.text = String(round(redSlider.value * 100) / 100)
+    }
+    
+    @IBAction func greenSliderAction() {
+        greenCounter.text = String(round(greenSlider.value * 100) / 100)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueCounter.text = String(round(blueSlider.value * 100) / 100)
+    }
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    
+    @IBAction func changeRGB() {
+        changeColor()
+    }
+    
+    
 
 }
 
